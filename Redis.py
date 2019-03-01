c3ce0ea1c70e9e06fe3167cf8c3eda13a45de711
Hayden351 <hayden.fields@gmail.com>
2019-02-28 23:00:04 -0500

    Redis documentation for python.

---
+++ Redis.py
@@ -0,0 +1,10 @@
+# https://stackoverflow.com/questions/25960695/why-python-using-redis-is-so-slow
+# https://github.com/andymccurdy/redis-py/blob/master/tests/test_pipeline.py
+# https://redis.io/topics/benchmarks
+# https://redis.io/topics/pipelining
+
+# transactions
+# pipelines
+# mget
+# https://stackoverflow.com/questions/29327544/pipelining-vs-transaction-in-redis
+# https://tech.trivago.com/2017/01/25/learn-redis-the-hard-way-in-production/
